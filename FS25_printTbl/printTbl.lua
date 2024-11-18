9eec6d9dfb15a370e5eabc41b30ec82abc75f61f
Rockstar94FS <112953821+Rockstar94FS@users.noreply.github.com>
2024-11-18 10:04:05 +0100

    Add files via upload

---
+++ FS25_printTbl/printTbl.lua
@@ -0,0 +1,30 @@
+printTbl = {
+	modName = g_currentModName
+}
+
+function printTbl:loadMap(name)
+	getmetatable(_G).__index[printTbl.modName].loadstring = nil
+	addConsoleCommand("_gsPrintTable", "", "consoleCommandPrintTable", self)
+end
+
+function printTbl:deleteMap()
+	removeConsoleCommand("_gsPrintTable")
+end
+
+function printTbl:consoleCommandPrintTable(inputTable, depth, maxDepth)
+	if inputTable ~= nil then
+		depth = depth or 0
+		maxDepth = maxDepth or 0
+
+		print("")
+		print("")
+		print("")
+		print("===================================================")
+		loadstring(string.format("DebugUtil.printTableRecursively(%s, '-', %d ,%d)", inputTable, depth, maxDepth))()
+		print("===================================================")
+	else
+		return "Usage: _gsPrintTable <table>"
+	end
+end
+
+addModEventListener(printTbl)
